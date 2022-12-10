de1e16dfadaae4eb07eee77c38eb98b03ad6f376
unknown <marko.rodriguez@hotmail.es>
2022-12-09 23:18:22 -0500

    Class validator DTO Auth

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,9 @@
-export interface AuthDto {
+import { IsEmail } from "class-validator";
+import { IsNotEmpty } from "class-validator/types/decorator/decorators";
+
+export class AuthDto {
+  @IsEmail()
   email: string;
+  @IsNotEmpty()
   password: string;
 }
